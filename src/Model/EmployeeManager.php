13f5d236a28d2361583c5e7f816146815b67d88b
glerique <gael.lerique@gmail.com>
2021-05-28 10:57:21 +0200

    Update Class with departement Class

--- src/Model/EmployeeManager.php
+++ src/Model/EmployeeManager.php
@@ -22,7 +22,7 @@ class EmployeeManager
         public function findAll()
         {
                 $req = $this->db->prepare('SELECT id, last_name, first_name, DATE_FORMAT(birth_date, "%d/%m/%Y") AS birth_date,
-                                                         DATE_FORMAT(hire_date, "%d/%m/%Y") AS hire_date, salary, departement FROM employee');
+                                                         DATE_FORMAT(hire_date, "%d/%m/%Y") AS hire_date, salary, departementId FROM employee');
                 $req->execute();
                 $req->setFetchMode(PDO::FETCH_CLASS, 'App\Entity\Employee');
                 $employees =  $req->fetchAll();
@@ -32,7 +32,7 @@ class EmployeeManager
         public function findById($id)
         {
 
-                $req = $this->db->prepare('SELECT id, last_name, first_name, birth_date, hire_date, salary, departement FROM employee WHERE id = :id');
+                $req = $this->db->prepare('SELECT id, last_name, first_name, birth_date, hire_date, salary, departementId FROM employee WHERE id = :id');
                 $req->bindValue(':id', (int)$id);
                 $req->execute();
                 $req->setFetchMode(PDO::FETCH_CLASS, 'App\Entity\Employee');
@@ -42,27 +42,27 @@ class EmployeeManager
 
         public function add(Employee $employee)
         {
-                $req = $this->db->prepare('INSERT INTO employee (last_name, first_name, birth_date, hire_date, salary, departement) 
-                VALUES (:last_name, :first_name, :birth_date, :hire_date, :salary, :departement)');
+                $req = $this->db->prepare('INSERT INTO employee (last_name, first_name, birth_date, hire_date, salary, departementId) 
+                VALUES (:last_name, :first_name, :birth_date, :hire_date, :salary, :departementId)');
                 $req->bindvalue(':last_name', $employee->getLastName());
                 $req->bindvalue(':first_name', $employee->getFirstName());
                 $req->bindvalue(':birth_date', $employee->getBirthDate());
                 $req->bindvalue(':hire_date', $employee->getHiredate());
                 $req->bindvalue(':salary', $employee->getSalary());
-                $req->bindvalue(':departement', $employee->getDepartement());
+                $req->bindvalue(':departementId', $employee->getDepartementId());
                 $req->execute();
         }
 
         public function update(Employee $employee)
         {
                 $req = $this->db->prepare('UPDATE employee SET last_name = :last_name, first_name = :first_name, birth_date = :birth_date,   
-                                                 hire_date = :hire_date, salary = :salary, departement = :departement WHERE id = :id');
+                                                 hire_date = :hire_date, salary = :salary, departementId = :departementId WHERE id = :id');
                 $req->bindvalue(':last_name', $employee->getLastName());
                 $req->bindvalue(':first_name', $employee->getFirstName());
                 $req->bindvalue(':birth_date', $employee->getBirthDate());
                 $req->bindvalue(':hire_date', $employee->getHiredate());
                 $req->bindvalue(':salary', $employee->getSalary());
-                $req->bindvalue(':departement', $employee->getDepartement());
+                $req->bindvalue(':departementId', $employee->getDepartementId());
                 $req->bindvalue(':id', $employee->getId());
                 $req->execute();
         }
