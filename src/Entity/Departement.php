ad9944d8851ca02654f7a23b1c266089f1f6a03e
glerique <gael.lerique@gmail.com>
2021-05-28 10:45:11 +0200

    Create Departement Class

---
+++ src/Entity/Departement.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+
+class Departement
+{
+
+    private $id;
+    private $name;
+
+
+
+    /**
+     * Getters
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * Setters
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $id;
+    }
+
+    public function setDepartement($name)
+    {
+        $this->name = $name;
+        return $name;
+    }
+    
+    public function __toString()
+    {
+        return $this->name;
+    }
+    
+}
