38b6a8a490dc21e228ef5674e426b96758c53885
glerique <gael.lerique@gmail.com>
2021-06-01 16:21:10 +0200

    Change button Class and minor changes

--- src/View/departement/nouveau.view.php
+++ src/View/departement/nouveau.view.php
@@ -6,7 +6,7 @@
 <section class="contact-section area-padding">
   <div class="container">
     <div class="row">      
-      <div class="col-lg-8">
+      <div class="col-lg-12">
         <form class="form-contact contact_form" method="post" action="/mvc-employees/departement/new">
           <div class="row">
             <div class="col-12">
@@ -16,7 +16,7 @@
             </div>
           </div>          
           <div class="form-group mt-3">
-            <input type="submit" class="button button-contactForm" name="ajouter" value="poster">
+            <input type="submit" class="btn btn-primary" name="ajouter" value="poster">
           </div>
         </form>
       </div>
