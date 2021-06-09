8c80f304d5e0154ddeca9f6bfb3eed00d89db7db
glerique <gael.lerique@gmail.com>
2021-06-09 16:10:29 +0200

    Refactor Class

--- src/Model/DepartementManager.php
+++ src/Model/DepartementManager.php
@@ -3,57 +3,39 @@
 namespace App\Model;
 
 use PDO;
-use Exception;
-use PDOStatement;
 use \App\Model\Bdd;
+use App\Model\Model;
 use App\Entity\Departement;
 
 
 
-class DepartementManager
+class DepartementManager extends Model
 {
+        protected $table = 'departement';
+        protected $class  = 'App\Entity\Departement';
 
-        private $db;
-
-        public function __construct()
-        {
-                $this->db = Bdd::dbConnect();
-        }
-
-        
-        public function findAll()
-        {
-                $req = $this->db->prepare('SELECT * FROM departement');
-                $req->execute();
-                $req->setFetchMode(PDO::FETCH_CLASS, 'App\Entity\Departement');
-                $departements =  $req->fetchAll();
-                return $departements;
-        }
 
         public function findById($id)
         {
-                
+
                 $req = $this->db->prepare('SELECT * FROM departement WHERE id = :id');
                 $req->bindValue(':id', (int)$id);
                 $req->execute();
                 $req->setFetchMode(PDO::FETCH_CLASS, 'App\Entity\Departement');
-                $departement =  $req->fetch();
-                return $departement;
-
-                
+                return $req->fetch();
         }
 
         public function add(Departement $departement)
         {
                 $req = $this->db->prepare('INSERT INTO departement (name) 
-                VALUES (:name)');                
+                VALUES (:name)');
                 $req->bindvalue(':name', $departement->getName());
                 $req->execute();
         }
 
         public function update(Departement $departement)
         {
-                $req = $this->db->prepare('UPDATE departement SET  name = :name WHERE id = :id');                
+                $req = $this->db->prepare('UPDATE departement SET  name = :name WHERE id = :id');
                 $req->bindvalue(':name', $departement->getName());
                 $req->bindvalue(':id', $departement->getId());
                 $req->execute();
@@ -61,9 +43,8 @@ class DepartementManager
 
         public function deleteById(Departement $departement)
         {
-                $req = $this->db->prepare('DELETE FROM departement WHERE id = :id');
+                $req = $this->db->prepare('DELETE FROM employee WHERE id = :id');
                 $req->bindvalue(':id', $departement->getId());
                 $req->execute();
-                
         }
 }
