35c7ebb22b1d83610dcc809baa608e4677bdd0ea
glerique <gael.lerique@gmail.com>
2021-06-10 12:51:40 +0200

    Add pagination and minor changes

--- src/View/employee/listing.view.php
+++ src/View/employee/listing.view.php
@@ -1,6 +1,4 @@
 <div class="container">
-    <p><b>Nombre total de salariés :</b> <?= $total ?></p>
-
 </div>
 <div class="container">
     <div class="row">
@@ -41,6 +39,24 @@
                     ?>
                 </tbody>
             </table>
+            <nav>
+                <ul class="pagination">
+                    <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+                    <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                        <a href="./<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+                    </li>
+                    <?php for ($page = 1; $page <= $pages; $page++) : ?>
+                        <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+                        <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                            <a href="./<?= $page ?>" class="page-link"><?= $page ?></a>
+                        </li>
+                    <?php endfor ?>
+                    <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+                    <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                        <a href="./<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
 </div>
